e848abee7e2195bd5eacee54ea3e47a69b19e3e6
TinasheChirimuuta07 <tinashechirimuuta@gmail.com>
2023-09-27 16:08:08 +0200

    new

---
+++ src/main/java/com/dreams/awss3service/AwsS3ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dreams.awss3service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsS3ServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsS3ServiceApplication.class, args);
+    }
+
+}
